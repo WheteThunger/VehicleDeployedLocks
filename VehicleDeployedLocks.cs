1f6e31694406914acc78f677d670c318263a6d1b
WhiteThunder <whetethunger@gmail.com>
2020-09-02 23:59:02 -0700

    Remove OnCarLockDelayedKill hook

--- VehicleDeployedLocks.cs
+++ VehicleDeployedLocks.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Vehicle Deployed Locks", "WhiteThunder", "0.1.0")]
+    [Info("Vehicle Deployed Locks", "WhiteThunder", "0.2.0")]
     [Description("Allows players to deploy code locks and key locks to vehicles.")]
     internal class VehicleDeployedLocks : CovalencePlugin
     {
@@ -165,10 +165,7 @@ namespace Oxide.Plugins
                 if (driverModule != null)
                     baseLock.SetParent(driverModule);
                 else
-                {
                     baseLock.Kill();
-                    Interface.CallHook("OnCarLockDelayedKill", car);
-                }
             });
         }
 
